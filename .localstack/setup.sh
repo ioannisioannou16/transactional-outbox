e66c7c4cdb8bbe1d0cfb561d0fe36f16aa5e0527
Ioannis Ioannou <ioannisioannou16@gmail.com>
2021-06-09 20:39:26 +0100

    start two services and add sns filters

--- .localstack/setup.sh
+++ .localstack/setup.sh
@@ -3,14 +3,26 @@
 topic_arn=$(awslocal sns create-topic --name test_topic --output text)
 echo "Topic ARN: $topic_arn"
 
-queue_arn=$(awslocal sqs create-queue --queue-name test_queue --output text)
-echo "Queue ARN: $queue_arn"
+queue_arn_1=$(awslocal sqs create-queue --queue-name test_queue_1 --output text)
+echo "Queue ARN 1: $queue_arn_1"
 
-subscription_arn=$(awslocal sns subscribe \
+queue_arn_2=$(awslocal sqs create-queue --queue-name test_queue_2 --output text)
+echo "Queue ARN 2: $queue_arn_2"
+
+subscription_arn_1=$(awslocal sns subscribe \
     --topic-arn "$topic_arn" \
     --protocol sqs \
-    --notification-endpoint "$queue_arn" \
+    --notification-endpoint "$queue_arn_1" \
+    --attributes '{"FilterPolicy":"{\"eventType\":[\"EmployeeCreated\"]}"}' \
     --output text)
 
-echo "Subscription ARN: $subscription_arn"
+echo "Subscription ARN 1: $subscription_arn_1"
 
+subscription_arn_2=$(awslocal sns subscribe \
+    --topic-arn "$topic_arn" \
+    --protocol sqs \
+    --notification-endpoint "$queue_arn_2" \
+    --attributes '{"FilterPolicy":"{\"eventType\":[\"EmployeeDeleted\"]}"}' \
+    --output text)
+
+echo "Subscription ARN 2: $subscription_arn_2"
